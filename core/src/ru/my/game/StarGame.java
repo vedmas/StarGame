40610a365ba62199744700baf93a99c178595941
vedmas <07011984t@mail.ru>
2019-07-16 21:53:51 +0500

    Revert "Сделал управляемое НЛО"
    This reverts commit c3f0b295af8b53c5a4d367a73df5fdb42a105cf4.

--- core/src/ru/my/game/StarGame.java
+++ core/src/ru/my/game/StarGame.java
@@ -1,18 +1,33 @@
 package ru.my.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
-import ru.my.game.Screen.MenuScreen;
-
-public class StarGame extends Game {
+public class StarGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	
 	@Override
-	public void create() {
-		setScreen(new MenuScreen());
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("Cosmos.jpg");
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(0f, 0f, 0.139f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
 	}
 }
