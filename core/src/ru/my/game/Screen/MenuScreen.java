85e6750089db8c9b0571b4047427e30d87e2d0f7
vedmas <07011984t@mail.ru>
2019-07-21 23:50:43 +0500

    Сделал спрайт, отцентровал движение НЛО

--- core/src/ru/my/game/Screen/MenuScreen.java
+++ core/src/ru/my/game/Screen/MenuScreen.java
@@ -4,48 +4,39 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.my.game.base.BaseScreen;
+import ru.my.game.sprite.Nlo;
 
 public class MenuScreen extends BaseScreen {
 
-    private static final float SPEED_LEN = 0.005f;
+
 
     private Texture ship, background;
-    private Vector2 click;
-    private Vector2 speed;
-    private Vector2 pos;
-    private Vector2 buf;
+    private Nlo nlo;
+
 
     @Override
     public void show() {
         super.show();
         background = new Texture("Cosmos.jpg");
         ship = new Texture("NLO.jpg");
-        click = new Vector2();
-        speed = new Vector2();
-        pos = new Vector2();
-        buf = new Vector2();
+        nlo = new Nlo(new TextureRegion(ship));
+
+
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
-        //pos.add(speed);
-        buf.set(click);
-        if(buf.sub(pos).len() > SPEED_LEN) {
-            pos.add(speed);
-        }
-        else {
-            pos.set(click);
-        }
-
+        nlo.update(delta);
         Gdx.gl.glClearColor(0.26f, 0.5f, 0.8f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         batch.draw(background, -1f, -1f, 2f, 2f);
-        batch.draw(ship, pos.x, pos.y, 0.15f, 0.15f);
+        nlo.draw(batch);
         batch.end();
     }
 
@@ -58,8 +49,7 @@ public class MenuScreen extends BaseScreen {
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
-        click.set(touch);
-        speed.set(click.cpy().sub(pos)).setLength(SPEED_LEN);
+        nlo.touchDown(touch, pointer, button);
         return false;
     }
 
