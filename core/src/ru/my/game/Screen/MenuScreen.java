2721bcac193d538ad4faeba8111ca00a0ac4ac2a
vedmas <07011984t@mail.ru>
2019-07-22 11:34:15 +0500

    Работаем дальше

--- core/src/ru/my/game/Screen/MenuScreen.java
+++ core/src/ru/my/game/Screen/MenuScreen.java
@@ -3,61 +3,57 @@ package ru.my.game.Screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.my.game.base.BaseScreen;
-import ru.my.game.sprite.Nlo;
+import ru.my.game.math.Rect;
+import ru.my.game.sprite.Background;
 
 public class MenuScreen extends BaseScreen {
 
-
-
-    private Texture ship, background;
-    private Nlo nlo;
-
+    private Texture backg;
+    private Background background;
 
     @Override
     public void show() {
         super.show();
-        background = new Texture("Cosmos.jpg");
-        ship = new Texture("NLO.jpg");
-        nlo = new Nlo(new TextureRegion(ship));
+        backg = new Texture("textures/Backg.jpg");
+        background = new Background(new TextureRegion(backg));
+    }
 
-
+    @Override
+    public void resize(Rect worldBounds) {
+        background.resize(backg);
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
-        nlo.update(delta);
-        Gdx.gl.glClearColor(0.26f, 0.5f, 0.8f, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        batch.draw(background, -1f, -1f, 2f, 2f);
-        nlo.draw(batch);
-        batch.end();
+        update();
+        draw();
     }
 
     @Override
     public void dispose() {
         super.dispose();
-        background.dispose();
-        ship.dispose();
+        backg.dispose();
     }
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
-        nlo.touchDown(touch, pointer, button);
         return false;
     }
 
-    //    @Override
-//    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-//        touch.set(screenX, Gdx.graphics.getHeight() - screenY);
-//        System.out.println("touch X = " + touch.x + " touch Y = " + touch.y);
-//        speed.set(touch.cpy().sub(pos)).setLength(SPEED_LEN);
-//        return false;
-//    }
+    public void update() {
+
+    }
+
+    public void draw() {
+        Gdx.gl.glClearColor(0.26f, 0.5f, 0.8f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.end();
+    }
 }
